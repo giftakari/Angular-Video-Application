776111f9f9006d8417440aeafe547c00202f0d9f
Godsgift ThankGod Akari <giftakari@yahoo.com>
2023-12-12 17:32:24 +1100

    ad components to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { VideoUploadComponent } from './video-upload/video-upload.component';
+import { VideoDisplayComponent } from './video-display/video-display.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    VideoUploadComponent,
+    VideoDisplayComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatCardModule,
+    MatToolbarModule, MatIconModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/video-display/video-display.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-video-display',
+  templateUrl: './video-display.component.html',
+  styleUrls: ['./video-display.component.css']
+})
+export class VideoDisplayComponent {
+
+}
